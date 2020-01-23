c730dfd6667678cec3f082070398ff3520990472
macwho <ganabbisativa@hotmail.com>
2020-01-22 18:06:20 -0600

    auth

--- baseProject/routes/api.php
+++ baseProject/routes/api.php
@@ -13,6 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// 
+// Route::get('login', function() {
+//     echo 'login';
+// });
+//
+// Route::middleware(['auth:api'])->group(function () {
+//     Route::get('show_users', 'UsersController@login');
+// });
+
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
